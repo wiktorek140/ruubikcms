e31cf26dacae004d891de725b660655f923dc3a4
Wiktor Strzębała <wiktorek140@gmail.com>
2025-01-04 03:05:55 +0100

    Rewrite with AI

--- ruubikcms/download.php
+++ ruubikcms/download.php
@@ -1,112 +1,124 @@
 <?php
-// --- File download with authentication and logging
-// --- Sample call with optional new name: download.php?f=phptutorial.zip&fc=newname.zip
-
 
 if (strpos($_SERVER['REQUEST_URI'], 'download.php') !== false) die("Access Denied");
 
 require('includes/dbconfig.php');
 require('includes/commonfunc.php');
-$dbh = new PDO(PDO_DB_DRIVER.':'.PDO_DB_FOLDER.'/'.PDO_DB_NAME); // database connection object
 
-/*$site = array();
-$site = get_site_data();
-$siteroot = '/'.($site['siteroot'] != "" ? trim($site['siteroot'],'/').'/' : '');*/
+class FileDownloader {
+    private $dbh;
+    private $baseDir;
 
-// files directory
-define('BASE_DIR','useruploads/files/');
+    public function __construct($dbh) {
+        $this->dbh = $dbh;
+        $this->baseDir = 'useruploads/files/';
+        @set_time_limit(0);
+    }
 
-// make sure program execution doesn't time out
-@set_time_limit(0);
+    public function download($fileName, $newFileName = null) {
+        if (empty($fileName)) {
+            die("Please specify file name for download.");
+        }
 
-if (!isset($_GET['f']) OR empty($_GET['f'])) {
-	die("Please specify file name for download.");
+        $fname = basename($fileName);
+        $fname = rtrim($fname);
+        $fpath = $this->baseDir . $fname;
+
+        if (!is_file($fpath)) {
+            die("File does not exist. Make sure you specified correct file name.");
+        }
+
+        $fsize = filesize($fpath);
+        $mtype = $this->getMimeType($fpath);
+
+        if ($newFileName === null) {
+            $asfname = $fname;
+        } else {
+            $asfname = str_replace(array('"', "'", '\\', '/'), '', $newFileName);
+            if ($asfname === '') $asfname = 'NoName';
+        }
+
+        $this->setHeaders($mtype, $asfname, $fsize);
+        $this->outputFile($fpath);
+
+        $this->logDownload($fname);
+        $this->updateDownloadCounter($fname);
+    }
+
+    private function getMimeType($fpath) {
+        $mtype = '';
+
+        if (function_exists('mime_content_type')) {
+            $mtype = mime_content_type($fpath);
+        } elseif (function_exists('finfo_file')) {
+            $finfo = finfo_open(FILEINFO_MIME);
+            $mtype = finfo_file($finfo, $fpath);
+            finfo_close($finfo);
+        }
+
+        if ($mtype == '') {
+            $mtype = "application/force-download";
+        }
+
+        return $mtype;
+    }
+
+    private function setHeaders($mtype, $asfname, $fsize) {
+        header("Pragma: public");
+        header("Expires: 0");
+        header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+        header("Cache-Control: public");
+        header("Content-Description: File Transfer");
+        header("Content-Type: $mtype");
+        header("Content-Disposition: attachment; filename=\"$asfname\"");
+        header("Content-Transfer-Encoding: binary");
+        header("Content-Length: " . $fsize);
+    }
+
+    private function outputFile($fpath) {
+        $file = @fopen($fpath, "r");
+        if ($file) {
+            while (!feof($file)) {
+                print(fread($file, 1024 * 8));
+                flush();
+                if (connection_status() != 0) {
+                    @fclose($file);
+                    die();
+                }
+            }
+            @fclose($file);
+        }
+    }
+
+    private function logDownload($fname) {
+        $stmt = $this->dbh->prepare("INSERT INTO dl_log (filename, ip, time) VALUES (?, ?, ?)");
+        $stmt->bindParam(1, $fname);
+        $stmt->bindParam(2, $_SERVER['REMOTE_ADDR']);
+        $stmt->bindParam(3, date("Y-m-d H:i:s"));
+        $stmt->execute();
+    }
+
+    private function updateDownloadCounter($fname) {
+        $dlcount = query_single("SELECT downloads FROM dl_count WHERE filename = '".$fname."'");
+        $date = date("Y-m-d H:i:s");
+        if (!$dlcount) {
+            $i = 1;
+            $stmt = $this->dbh->prepare("INSERT INTO dl_count (filename, downloads, count_started, last_dl) VALUES (?, ?, ?, ?)");
+            $stmt->bindParam(1, $fname);
+            $stmt->bindParam(2, $i);
+            $stmt->bindParam(3, $date);
+            $stmt->bindParam(4, $date);
+            $stmt->execute();
+        } else {
+            $stmt = $this->dbh->prepare("UPDATE dl_count SET downloads = ? + 1, last_dl = ? WHERE filename = ?");
+            $stmt->bindParam(1, $dlcount);
+            $stmt->bindParam(2, $date);
+            $stmt->bindParam(3, $fname);
+            $stmt->execute();
+        }
+    }
 }
 
-// get real file name, remove any path info to avoid hacking by adding relative path etc
-$fname = basename($_GET['f']);
-$fname = rtrim($fname);
-$fpath = BASE_DIR.$fname;
-
-if (!is_file($fpath)) {
-	die("File does not exist. Make sure you specified correct file name.");
-}
-
-// file size in bytes
-$fsize = filesize($fpath); 
-
-// get mime type
-$mtype = '';
-
-if (function_exists('mime_content_type')) {
-	$mtype = mime_content_type($fpath);
-} elseif (function_exists('finfo_file')) {
-	$finfo = finfo_open(FILEINFO_MIME); // return mime type
-	$mtype = finfo_file($finfo, $fpath);
-	finfo_close($finfo);  
-}
-
-if ($mtype == '') {
-	$mtype = "application/force-download";
-}
-
-// override original filename with given (optional fc)
-if (!isset($_GET['fc']) OR empty($_GET['fc'])) {
-	$asfname = $fname;
-} else {
-	// remove some bad chars
-	$asfname = str_replace(array('"',"'",'\\','/'), '', $_GET['fc']);
-	if ($asfname === '') $asfname = 'NoName';
-}
-
-// set headers
-header("Pragma: public");
-header("Expires: 0");
-header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-header("Cache-Control: public");
-header("Content-Description: File Transfer");
-header("Content-Type: $mtype");
-header("Content-Disposition: attachment; filename=\"$asfname\"");
-header("Content-Transfer-Encoding: binary");
-header("Content-Length: " . $fsize);
-
-// download
-$file = @fopen($fpath,"r");
-if ($file) {
-	while(!feof($file)) {
-		print(fread($file, 1024*8));
-		flush();
-		if (connection_status()!=0) {
-			@fclose($file);
-			die();
-		}
-	}
-	@fclose($file);
-}
-
-// log download
-$stmt = $dbh->prepare("INSERT INTO dl_log (filename, ip, time) VALUES (?, ?, ?)");
-$stmt->bindParam(1, $fname);
-$stmt->bindParam(2, $_SERVER['REMOTE_ADDR']);
-$stmt->bindParam(3, date("Y-m-d H:i:s"));
-$stmt->execute();
-
-// add or increase download counter
-$dlcount = query_single("SELECT downloads FROM dl_count WHERE filename = '".$fname."'");
-$date = date("Y-m-d H:i:s");
-if (!$dlcount) {
-	$i = 1;
-	$stmt = $dbh->prepare("INSERT INTO dl_count (filename, downloads, count_started, last_dl) VALUES (?, ?, ?, ?)");
-	$stmt->bindParam(1, $fname);
-	$stmt->bindParam(2, $i);
-	$stmt->bindParam(3, $date);
-	$stmt->bindParam(4, $date);
-	$stmt->execute();
-} else {
-	$stmt = $dbh->prepare("UPDATE dl_count SET downloads = ? + 1, last_dl = ? WHERE filename = ?");
-	$stmt->bindParam(1, $dlcount);
-	$stmt->bindParam(2, $date);
-	$stmt->bindParam(3, $fname);
-	$stmt->execute();
-}
-?>
+$dbh = new PDO(PDO_DB_DRIVER . ':' . PDO_DB_FOLDER . '/' . PDO_DB_NAME);
+$downloader = new FileDownloader($dbh);
+$downloader->download($_GET['f'], $_GET['fc'] ?? null);
