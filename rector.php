648014af757a5448c36667b3e1b04c89b9ed0295
wiktorek140 <wiktorek140@gmail.com>
2025-01-08 22:51:59 +0100

    rector to 8.3

--- rector.php
+++ rector.php
@@ -12,7 +12,7 @@ return RectorConfig::configure()
 	  __DIR__ . '/index.php'
 	])
 	// uncomment to reach your current PHP version
-	->withPhpSets()
+	->withPhpSets(php83: true)
 	->withTypeCoverageLevel(0)
 	->withDeadCodeLevel(0)
 	->withCodeQualityLevel(0);
