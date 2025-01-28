028406141a300ff2f296bb067dc73edd942cacb0
wiktorek140 <wiktorek140@gmail.com>
2025-01-28 07:05:46 +0100

    doctype manager

---
+++ tests/DoctypeManagerTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Ruubik\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Ruubik\Service\Utils\DoctypeManager;
+
+class DoctypeManagerTest extends TestCase
+{
+    public function testGetDoctype(): void
+    {
+        $doctypeManager = new DoctypeManager();
+
+        // Valid doctype ID
+        $this->assertEquals(
+            '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">',
+            $doctypeManager->getDoctype(1)
+        );
+
+        // Invalid doctype ID
+        $this->assertNull($doctypeManager->getDoctype(99));
+    }
+
+    public function testGetAllDoctypes(): void
+    {
+        $doctypeManager = new DoctypeManager();
+
+        $expectedDoctypes = [
+            1  => '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">',
+            2  => '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">',
+            3  => '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">',
+            4  => '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">',
+            5  => '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML Basic 1.0//EN" "http://www.w3.org/TR/xhtml-basic/xhtml-basic10.dtd">',
+            6  => '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML Basic 1.1//EN" "http://www.w3.org/TR/xhtml-basic/xhtml-basic11.dtd">',
+            7  => '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">',
+            8  => '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">',
+            9  => '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">',
+            10 => '<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.2//EN" "http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd">',
+            11 => '<!DOCTYPE HTML>',
+        ];
+
+        $this->assertEquals($expectedDoctypes, $doctypeManager->getAllDoctypes());
+    }
+}
